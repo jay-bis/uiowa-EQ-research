ad28cb25c9c5b7bd05dbf90a131dc4fdf121d09b
Jack Biscupski <jack-biscupski@uiowa.edu>
2018-03-02 23:27:22 -0600

    Update runSW.py
    added comments/doc for clarity

--- src/driver/runSW.py
+++ src/driver/runSW.py
@@ -1,7 +1,16 @@
-####### initial portion of this driver creates and sets up grids to be manipulated in ICP ##########
-import time
+"""
+This is meant to be the 'driver' program for the ICP code. The main function of this program
+is: load in pre/post event DEMs, process them and extract pertinent data, create grids for re-
+sampling, populate the grids with resampled data, run the ICP code over the entire grid, and
+finally create new, shape-matched post event DEM.
+
+@author: Jack Biscupski
+"""
+
 from osgeo import gdal
 import numpy as np
+import time
+
 
 preEQ = "C:/Users/Jack Biscupski/Desktop/Research Materials/Code/WV01_13OCT01n14JAN24_10200100262F9000_1020010029seg1_2m_dem_1.tif"
 postEQ = "C:/Users/Jack Biscupski/Desktop/Research Materials/Code/WWV01_12MAY04n13APR01_102001001CAFAC00_1020010020seg1_2m_dem_1.tif"
@@ -16,9 +25,10 @@ postEQclamp = 30
 # loading and preprocessing data
 ds_pre = gdal.Open(preEQ)
 ds_post = gdal.Open(postEQ)
+# convert geotiffs to arrays for manipulation
 arr_pre, arr_post = ds_pre.ReadAsArray(), ds_post.ReadAsArray()
-pre_transform, post_transform = ds_pre.GetGeoTransform(), ds_post.GetGeoTransform()         # provides info about cell resolution and x/y extent
-preShape, postShape = np.shape(arr_pre), np.shape(arr_post)                                  # provides info about dimensions of loaded geotiff array
+pre_transform, post_transform = ds_pre.GetGeoTransform(), ds_post.GetGeoTransform() # provides info about cell resolution and x/y extent
+preShape, postShape = np.shape(arr_pre), np.shape(arr_post)                                  
 
 # initialize x/y extents for ease of use
 xExtentMinPre = pre_transform[0]
@@ -43,15 +53,18 @@ preX, preY = np.meshgrid(np.arange(pre[0]+0.5, pre[1], pre_transform[1]), np.ara
 postX, postY = np.meshgrid(np.arange(post[0]+0.5, post[1], post_transform[1]), np.arange(post[2]+0.5, post[3], post_transform[1]))
 
 # fill in values for pre/post matrices
+# NumPy library automatically casts missing/Nan values as -9999
 preM[0,:], postM[0,:] = np.reshape(np.transpose(preX), preShape[0]*preShape[1]), np.reshape(np.transpose(postX), postShape[0]*postShape[1])
 preM[1,:], postM[1,:] = np.reshape(np.transpose(preY), preShape[0]*preShape[1]), np.reshape(np.transpose(postY), postShape[0]*postShape[1])
-preM[2,:], postM[2,:] = np.reshape(np.transpose(arr_pre), preShape[0]*postShape[1]), np.reshape(np.transpose(arr_post), postShape[0]*postShape[1])  # missing values are already cast as -9999
+preM[2,:], postM[2,:] = np.reshape(np.transpose(arr_pre), preShape[0]*postShape[1]), np.reshape(np.transpose(arr_post), postShape[0]*postShape[1])  
 
+# used to create max/min extents of resampled grid
 minX = min(pre[0], post[0])
 maxX = max(pre[1], post[1])
 maxY = max(pre[3], post[3])
 minY = min(pre[2], post[2])
 
+# resolution of resampled grid
 x_Range = np.linspace(minX, maxX+1, int((maxX-minX)/slidingWindow)+1)
 y_Range = np.linspace(minY, maxY+1, int((maxY-minY)/slidingWindow)+1)
 
@@ -60,17 +73,16 @@ gridPre, gridPost, gridPreAligned = [], [], []
 # time grid resampling - testing purposes only
 print("Timing resampling...")
 start = time.time()
-# for [:,I_pre] and [:,J_pre], etc - this means we are going through each matrix prefixed (for example, preM/postM or temps)
+# for [:,I_pre] and [:,J_pre], etc - we are going through each matrix prefixed (for example, preM/postM or temps)
 # and picking out the values by starting at the beginning of I_ and J_ and saying: if True, keep this 3x1 element, if not, discard
-# 
 for i in range(len(x_Range)-1):
     # initiate empty list for each entry
     gridPre.append([])
     gridPost.append([])
     gridPreAligned.append([])
     # resample x coordinates
-    I_pre = (preM[0,:] >= x_Range[i]) & (preM[0,:] < (x_Range[i] + windowSize))    
-    I_post = (postM[0,:] >= (x_Range[i] - postEQclamp)) & (postM[0,:] < (x_Range[i] + windowSize + postEQclamp))
+    I_pre = np.logical_and((preM[0,:] >= x_Range[i]), (preM[0,:] < (x_Range[i] + windowSize)))    
+    I_post = np.logical_and((postM[0,:] >= (x_Range[i] - postEQclamp)), (postM[0,:] < (x_Range[i] + windowSize + postEQclamp)))
     
     tempData_pre = preM[:,I_pre]
     tempData_post = postM[:,I_post]
@@ -79,14 +91,16 @@ for i in range(len(x_Range)-1):
         gridPost[i].append([])
         gridPreAligned[i].append([])
         # resample y coordinates
-        J_pre = (tempData_pre[1,:] >= y_Range[j]) & (tempData_pre[1,:] < (y_Range[j] + windowSize))
-        J_post = (tempData_post[1,:] >= (y_Range[j] - postEQclamp)) & (tempData_post[1,:] < (y_Range[j] + windowSize + postEQclamp))
+        J_pre = np.logical_and((tempData_pre[1,:] >= y_Range[j]), (tempData_pre[1,:] < (y_Range[j] + windowSize)))
+        J_post = np.logical_and((tempData_post[1,:] >= (y_Range[j] - postEQclamp)), (tempData_post[1,:] < (y_Range[j] + windowSize + postEQclamp)))
         # create each grid point/cell
         gridPre[i][j] = tempData_pre[:,J_pre]
         gridPost[i][j] = tempData_post[:,J_post]
-end = time.time()    # takes 37-39 mins to do it this way???
+# based on personal tests so far, takes anywhere from 37-42 mins        
+end = time.time()    
 print("Resampling elapsed time: {} seconds".format(end-start))
 
 # create multiple zero-instantiated arrays with same shape as gridPreAligned
 Tx, TTx, Ty, TTy, Tz, TTz = (np.zeros((len(x_Range)-1, len(y_Range)-1)) for i in range(6)) # what are these T arrays for?
+counter = 1
 Tr = np.zeros((9, (len(x_Range)-1)*(len(y_Range)-1)))
