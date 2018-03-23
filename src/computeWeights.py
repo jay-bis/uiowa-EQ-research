2f20a76c0f4f78e2c31dc03c2fc34e5137428798
Jack Biscupski <jack-biscupski@uiowa.edu>
2018-03-23 12:21:39 -0500

    merge with ICP.py later

--- src/computeWeights.py
+++ src/computeWeights.py
@@ -1,2 +1,11 @@
+"""
+INPUTS: dist - magnitude of closest points?? (double)
+        p - pNorm (integer)
+OUTPUTS: weights 
+
+@author: Jack Biscupski
+"""
+
 def computeWeights(dist, p):
-    return 0
+    reg = 1e-8
+    return(p/(dist^(2-p) + reg))
